f897cdaf364cc1b426c6b5d58082f7e51385d137
ponymushama <ponymushama@gmail.com>
2025-01-05 21:43:02 +0800

    use yazi flavor

--- home.nix
+++ home.nix
@@ -325,7 +325,6 @@
     '';
 
   # yazi
-
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
@@ -334,6 +333,25 @@
       manager = {
         show_hidden = true;
       };
+    };
+    flavors = {
+      catppuccin-mocha = let
+        src = pkgs.fetchFromGitHub {
+          owner = "yazi-rs";
+          repo = "flavors";
+          rev = "fc8eeaab9da882d0e77ecb4e603b67903a94ee6e";
+          hash = "sha256-wvxwK4QQ3gUOuIXpZvrzmllJLDNK6zqG5V2JAqTxjiY=";
+        };
+      in pkgs.runCommand "yazi-flavor-catppuccin-mocha" { } ''
+        mkdir -p $out
+        cp -r ${src}/catppuccin-mocha.yazi/* $out/
+      '';
+    };
+    theme = {
+      flavor = {
+        dark = "catppuccin-mocha";
+        light = "catppuccin-mocha";
+    };
     };
   };
 
