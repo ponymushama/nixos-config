a023cbdad548ea145206ab697ee9fc769d6ed39b
ponymushama <ponymushama@gmail.com>
2025-01-07 21:40:14 +0800

    config python dev

--- flake.nix
+++ flake.nix
@@ -1,22 +1,54 @@
 {
   description = "ponymushama nix-config";
 
+  # define input source
   inputs = {
+    # main nixpkgs repository
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home manager for user configuration
     home-manager = {
       url = "github:nix-community/home-manager";
+      # keep home manager sync with main nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # utils for python
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, utils, ... }: let
-    # Python 开发环境配置
-    pythonEnv = system: let
+  outputs = inputs@{ self, nixpkgs, home-manager, utils, ... }: 
+    {
+      nixosConfigurations = {
+        # host
+        ponymushama = let
+          username = "ponymushama";
+          specialArgs = {inherit username;};
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          system = "x86_64-linux";
+          # modules
+          modules = [
+            # system
+            ./configuration.nix
+            # home
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ponymushama = import ./home-manager/home.nix;
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+            }
+          ];
+        };
+      };
+    }
+    # python
+    # https://github.com/MordragT/nix-templates/tree/master/python-venv
+    // (utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       pythonPackages = pkgs.python3Packages;
     in {
-      default = pkgs.mkShell {
+      devShells.default = pkgs.mkShell {
         name = "python-venv";
         venvDir = "./.venv";
         buildInputs = [
@@ -34,31 +66,5 @@
           unset SOURCE_DATE_EPOCH
         '';
       };
-    };
-  in {
-    # 系统配置
-    nixosConfigurations = {
-      ponymushama = let
-        username = "ponymushama";
-        specialArgs = {inherit username;};
-      in
-      nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ponymushama = import ./home-manager/home.nix;
-            home-manager.extraSpecialArgs = inputs // specialArgs;
-          }
-        ];
-      };
-    };
-
-    # 开发环境
-    devShells = utils.lib.eachDefaultSystem (system: pythonEnv system);
-  };
+    }));
 }
