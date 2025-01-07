28e649051ac94cf842a961717cd0ab07bf8262db
ponymushama <ponymushama@gmail.com>
2025-01-07 13:46:36 +0800

    re-write flake.nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,39 @@
 {
   description = "ponymushama nix-config";
 
+  # define input source
   inputs = {
+    # main nixpkgs repository
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager, used for managing user configuration
+    # home manager for user configuration
     home-manager = {
       url = "github:nix-community/home-manager";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
+      # keep home manager sync with main nixpkgs
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    # define system config
     nixosConfigurations = {
+      # host
       ponymushama = let
         username = "ponymushama";
         specialArgs = {inherit username;};
       in
-      # 这里的 my-nixos 替换成你的主机名称
       nixpkgs.lib.nixosSystem {
         inherit specialArgs;
         system = "x86_64-linux";
+        # modules
         modules = [
+          # system
           ./configuration.nix
-
-          # 将 home-manager 配置为 nixos 的一个 module
-          # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
+          # home
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.ponymushama = import ./home-manager/home.nix;
             home-manager.extraSpecialArgs = inputs // specialArgs;
           }
