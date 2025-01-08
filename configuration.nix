243ccf5e585e5ca3574eb8198e93c6d13a5f263f
ponymushama <ponymushama@gmail.com>
2025-01-08 16:50:37 +0800

    re-write config

--- configuration.nix
+++ configuration.nix
@@ -10,32 +10,34 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.substituters = [
     # 中国镜像
-    "https://mirrors.sustech.edu.cn/nix-channels/store"
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    # 社区缓存
+    # "https://mirrors.sustech.edu.cn/nix-channels/store"
+    # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://nix-community.cachix.org"
   ];
 
-  # Bootloader.
+  # bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "ponymushama"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # hostname
+  networking.hostName = "ponymushama";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # network
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # TUN/TAP
+  boot.kernelModules = [ "tun" ];
+  # TUN/TAP mihomo-party
+  security.wrappers.mihomo-party = {
+    source = "${pkgs.mihomo-party}/bin/mihomo-party";
+    capabilities = "cap_net_admin,cap_net_bind_service=+ep";
+    owner = "root";
+    group = "wheel";
+  };
+
+  # localization
   time.timeZone = "Asia/Shanghai";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "zh_CN.UTF-8";
     LC_IDENTIFICATION = "zh_CN.UTF-8";
@@ -45,29 +47,29 @@
     LC_NUMERIC = "zh_CN.UTF-8";
     LC_PAPER = "zh_CN.UTF-8";
     LC_TELEPHONE = "zh_CN.UTF-8";
-    LC_TIME = "zh_CN.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.{
+  # X11
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # GNOME desktop
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
+  # config keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
     options = "ctrl:nocaps";
   };
-  # make keymap config same between x11 console wayland
+  # make keymap config same between x11 & console & wayland
   console.useXkbConfig = true;
 
-  # Enable CUPS to print documents.
+  # print
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # audio
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -75,42 +77,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # define user account
   users.users.ponymushama = {
     isNormalUser = true;
     description = "ponymushama";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
+  # unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # system packages
   environment.systemPackages = with pkgs; [
     git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
-    dconf
-    nix-ld
-    gnomeExtensions.kimpanel # fcitx5 wayland panel
   ];
 
   # nix-ld
@@ -138,7 +121,7 @@
     Defaults env_keep += "HOME XDG_CONFIG_HOME"
   '';
 
-  # fonts
+  # system fonts
   fonts = {
     fontDir.enable = true;
     packages = with pkgs; [
@@ -148,7 +131,7 @@
     ];
   };
 
-  # GNOME default fonts
+  # GNOME dconf settings
   programs.dconf.enable = true;
   services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.desktop.interface]
@@ -157,44 +140,9 @@
     monospace-font-name='JetBrainsMono Nerd Font Mono 11'
   '';
  
-  # fish
-  users.defaultUserShell = pkgs.fish;
+  # default shell = fish
   programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
 
-  # TUN/TAP
-  boot.kernelModules = [ "tun" ];
-  # TUN/TAP mihomo-party
-  security.wrappers.mihomo-party = {
-    source = "${pkgs.mihomo-party}/bin/mihomo-party";  # 修改为 mihomo-party
-    capabilities = "cap_net_admin,cap_net_bind_service=+ep";
-    owner = "root";
-    group = "wheel";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
