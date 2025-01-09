e5a0056a4f66e9a21a3a4760950be2c11e240fcc
ponymushama <ponymushama@gmail.com>
2025-01-09 19:00:08 +0800

    change fxitx5 rime config

--- home-manager/fcitx5/default.nix
+++ home-manager/fcitx5/default.nix
@@ -14,13 +14,23 @@
     # rime enable lua
     librime
     librime-lua
+    # fcitx5 theme
+    fcitx5-rose-pine
+    fcitx5-catppuccin
+    fcitx5-material-color
   ];
   # fcitx5 config
-  imports = [ ./fcitx5.nix ];
+  # imports = [ ./fcitx5.nix ];
+  home.file = {
+    ".config/fcitx5" = {
+      source = ./fcitx5-config;
+      recursive = true;
+    };
+  };
   # rime config
   home.file = {
     ".local/share/fcitx5/rime" = {
-      source = ./rime-data-ama/share/rime-data;
+      source = ./rime-config;
       recursive = true;
     };
   };
