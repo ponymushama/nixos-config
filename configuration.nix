a53d86fc96ced92f103f4161643d9bd5fede8feb
ponymushama <ponymushama@gmail.com>
2025-01-10 18:58:50 +0800

    config key & mirror

--- configuration.nix
+++ configuration.nix
@@ -8,12 +8,14 @@
 
   # flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.substituters = [
-    # 中国镜像
-    # "https://mirrors.sustech.edu.cn/nix-channels/store"
-    # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://nix-community.cachix.org"
-  ];
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      # "https://mirrors.sustech.edu.cn/nix-channels/store"
+      # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    ];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+  };
 
   # kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
